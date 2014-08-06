407d42f727711c71d7d3479d3ce77c62b92e2003
Francois Gaillard <francois.gaillard@prestashop.com>
2014-08-06 14:24:03 +0200

    Version changed (v1.0.5)

--- cronjobs.php
+++ cronjobs.php
@@ -39,7 +39,7 @@ class CronJobs extends PaymentModule
 	{
 		$this->name = 'cronjobs';
 		$this->tab = 'administration';
-		$this->version = '1.0.3';
+		$this->version = '1.0.5';
 		$this->module_key = '';
 
 		$this->currencies = true;
