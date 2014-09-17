41c3198c40326170f5c71d9fa330a64ed9bc0b1e
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-09-17 11:58:02 +0200

    // Version updated to v1.1.8

--- cronjobs.php
+++ cronjobs.php
@@ -46,7 +46,7 @@ class CronJobs extends PaymentModule
 	{
 		$this->name = 'cronjobs';
 		$this->tab = 'administration';
-		$this->version = '1.1.7';
+		$this->version = '1.1.8';
 		$this->module_key = '';
 
 		$this->controllers = array('callback');
